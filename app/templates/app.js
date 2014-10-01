84d2c5649113491e05e226a75299d264fe412ed8
Xiaolu Liu <xiaolu.liu@g-pc-3651.intra.gree-office.net>
2014-10-01 17:42:14 +0900

    version 0.0.3

--- app/templates/app.js
+++ app/templates/app.js
@@ -2,6 +2,11 @@
 /* global F */
 (function() {
   'use strict';
-  F.construct();
+  F.construct({
+    DomParser: 'bower_components/jquery/dist/jquery.min.js',
+    Template: {
+      Engine: 'bower_components/hogan/web/builds/3.0.2/hogan-3.0.2.min.js',
+    },
+  });
 })();
 
