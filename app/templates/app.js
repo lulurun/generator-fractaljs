c2107a3081dcf95773f13524307bfa3577627613
Xiaolu Liu <xiaolu.liu@g-pc-3651.intra.gree-office.net>
2014-10-01 16:48:34 +0900

    apply to fractaljs 1.1.x

--- app/templates/app.js
+++ app/templates/app.js
@@ -2,17 +2,6 @@
 /* global F */
 (function() {
   'use strict';
-  var App = F.App.extend({
-    init: function(){
-      this.DOM_PARSER = 'bower_components/jquery/dist/jquery.min.js';
-      this.TEMPLATE_ENGINE = 'bower_components/hogan/web/builds/3.0.2/hogan-3.0.2.min.js';
-      this.PREFIX = {
-        component: 'components/',
-        template: 'templates/'
-      };
-    },
-  });
-  var app = new App();
-  app.start();
+  F.construct();
 })();
 
